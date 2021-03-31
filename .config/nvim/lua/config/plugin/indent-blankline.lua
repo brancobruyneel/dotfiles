e487d08c6c274c1781b6ef983fdb9d4bd148c6a4
Branco <branco.bruyneel@gmail.com>
2021-03-31 13:41:26 +0200

    Update blankline indent

--- .config/nvim/lua/config/plugin/indent-blankline.lua
+++ .config/nvim/lua/config/plugin/indent-blankline.lua
@@ -1,7 +1,7 @@
 local g = vim.g
 
 g.indentLine_enabled = 1
-g.indent_blankline_char = "▏"
-g.indent_blankline_filetype_exclude = { "help", "terminal", "packer", "nvimtree" }
+g.indent_blankline_char = '│'
+g.indent_blankline_filetype_exclude = { "help", "terminal", "packer", "nvimtree", "TelescopePrompt" }
 g.indent_blankline_show_trailing_blankline_indent = false
-g.indent_blankline_show_first_indent_level = true
+g.indent_blankline_show_first_indent_level = false
