568d220268ebdee9f80243b3dd01f1e676b5080c
Branco <branco.bruyneel@gmail.com>
2022-09-04 20:28:02 +0200

    remove custom

--- .config/nvim/lua/custom/custom/init.lua
+++
@@ -1,4 +0,0 @@
-local opt = vim.opt
-
-opt.relativenumber = true
-opt.clipboard = ""
--- .config/nvim/lua/custom/custom/plugins/others.lua
+++
@@ -1,7 +0,0 @@
-local M = {}
-
-M.treesitter_context = function()
-  require("treesitter-context").setup {}
-end
-
-return M
